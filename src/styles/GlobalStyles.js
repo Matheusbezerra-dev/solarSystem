e578cdd4321baddc989737f22970777e205bd038
Matheus Bezerra <mabezerra2@gmail.com>
2023-03-25 15:22:20 -0400

    atualizando depedencias e resposividade

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -9,14 +9,8 @@ const GlobalStyled = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  html, body, :root {
-    min-height: 100%;
-  }
-
   body {
     display: flex;
-    /* justify-content: center;
-    flex-direction: column; */
     background-image: url(${imgbackground});
   }
 `;
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -4,17 +4,16 @@ export const DivApp = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  padding: 2rem;
 `;
 
 export const HeaderContainer = styled.header`
   display: flex;
-  justify-content: center;
-  
-  img {
-    width: 426px;
-    height: 243px;
-    margin-top: 40px;
-    margin-bottom: 74px;
+  justify-content: center;  
+  @media (max-width: 580px) {
+    img{
+      width: 280px;
+    }
   }
 `;
 
@@ -22,15 +21,21 @@ export const ContainerPlanets = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: row; 
+  /* flex-direction: row;  */
+  flex-wrap: wrap;
   color: white;
-
   div {
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column-reverse;
     margin-left: 20px;
+  }
+  @media (max-width: 480px) {
+    flex-direction: column;
+    div{
+      margin-left: 0;
+    }
   }
 `;
 
@@ -55,12 +60,20 @@ export const ContainerMissions = styled.div`
 export const DivMission = styled.section`
   background: linear-gradient(0deg, rgba(24, 26, 37, 0.5), rgba(24, 26, 37, 0.5));
   border: 1px solid rgba(255, 255, 255, 1);
-  width: 495px;
+  max-width: 495px;
+  width: 100%;
+  /* max-height: 250px;
+  height: 100%; */
   height: 190px;
   border-radius: 17px;
   margin: 10px;
   div {
     height: 50%;
+  }
+  @media (max-width: 480px) {
+    /* max-height: 250px;
+    height: 100%; */
+    height: 350px;
   }
  `;
 
@@ -84,8 +97,10 @@ export const DivMissionAside1 = styled.div`
   color: rgb(255 255 255);
   div{
     display: flex;
+    justify-content: center;
     align-items: center;
-    width: 450px;
+    max-width: 450px;
+    width: 100%;
     text-transform: uppercase;
     font-weight: 300;
     font-family: Epilogue;
@@ -95,18 +110,23 @@ export const DivMissionAside1 = styled.div`
 export const DivMissionAside = styled.div`
   display: flex;
   justify-content: space-around;
+  /* flex-wrap: wrap; */
   align-items: center;
-
   div {
     color: rgb(255 255 255);
     font-weight: 400;
     font-family: Epilogue;
+    flex-direction: column;
   }
   
   div img {
     width: 15px;
     height: 18px;
     margin-right: 5px;
+  }
+  @media (max-width: 480px) {
+    justify-content: center;
+    flex-direction: column;
   }
 `;
 
