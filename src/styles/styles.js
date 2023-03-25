f291044b97b039b335612315014371063cd61459
Matheus Bezerra <mabezerra2@gmail.com>
2023-03-25 15:38:29 -0400

    atualizando depedencias e resposividade

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -21,7 +21,7 @@ export const ContainerPlanets = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  /* flex-direction: row;  */
+  flex-direction: row; 
   flex-wrap: wrap;
   color: white;
   div {
